d4214a67b258db8c9fb51844912c84ebb97983b7
Wen Etchou <wenetchou330@gmail.com>
2026-04-26 10:42:01 +0100

    Résolution des épisodes 7 et 8

---
+++ src/js/episode-7.ts
@@ -0,0 +1,9 @@
+// Programme qui demande l'âge à l'utilisateur et l'indique s'il est mineur ou majeur
+const age : number = Number(prompt('Quel âge avez-vous ? ')); //On demande l'âge de l'utilisateur
+    if (age < 1 || age > 120) { // Si l'âge de l'utilisteur est inférieur à 1 ou supérieur à 120 on aficche un message d'erreur
+        alert('Entrée invaide');
+    } else if (age >= 1 && age <= 17) { //Si l'âge de l'utilisateur est compris entre 1 et 17 alors on lui dit qu'il est mineur
+        alert('Vous êtes mineur');
+     } else {                           // Au contraire si l'âge n'est pas dans l'intervalle précédent alors il est focément majeur
+        alert('Vous êtes majeur')
+        }
--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,6 @@
 // import './js/episode-3' //Importation de l'épisode 3
 // import './js/episode-4' //Importation de l'épisode 4
 // import './js/episode-5' //Importation de l'épisode 5
-import './js/episode-6' //Importation de l'épisode 6
+// import './js/episode-6' //Importation de l'épisode 6
+// import './js/episode-7' //Importation de l'épisode 7
+import './js/episode-8' //Importation de l'épisode 8
